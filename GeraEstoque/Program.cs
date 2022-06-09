bcc20cbbc914fdd326d8a6a1c22bf2bae99231e6
FridayAI <gabi.get@hotmail.com>
2022-06-09 14:59:56 -0300

    dotnet new console

---
+++ GeraEstoque/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GeraEstoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
