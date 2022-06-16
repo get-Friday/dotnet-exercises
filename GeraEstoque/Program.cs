bec0e6a6056ca00f45d9982daabafe09192ec7a4
FridayAI <gabi.get@hotmail.com>
2022-06-15 21:50:31 -0300

    add menuScreen method Iniciar

--- GeraEstoque/Program.cs
+++ GeraEstoque/Program.cs
@@ -1,65 +1,12 @@
-﻿namespace GeraEstoque
+﻿using GeraEstoque.Screens;
+
+namespace GeraEstoque
 {
     internal class Program
     {
-        static void CadastrarProduto()
-        {
-            Console.Clear();
-            Console.Write("Insira o nome do produto: ");
-            string product = Console.ReadLine();
-            Console.WriteLine("");
-            Console.Write("Sua quantidade em estoque: ");
-            int qntStock = int.Parse(Console.ReadLine());
-            Console.WriteLine("");
-            Console.Write("Seu valor de compra: ");
-            float price = float.Parse(Console.ReadLine());
-
-            Guid obj = Guid.NewGuid();
-
-            Console.WriteLine("");
-            Console.Write(@$"Produto Cadastrado com sucesso!
-            ID: {obj};
-            Nome: {product};
-            Qtd: {qntStock};
-            R$ Compra: {price.ToString("C")};
-            ");
-
-        }
-        static void Menu()
-        {
-            string textoMenu = @"Seja bem vindo ao GeraEstoque 1.0
-----------------------------------------
-1  Cadastrar produto
-2  Consultar produto
-3  Modificar produto
-4  Excluir produto
-0  Sair
-Digite a opção: ";
-            Console.Clear();
-            Console.Write(textoMenu);
-            var option = byte.Parse(Console.ReadLine());
-
-            switch (option)
-            {
-                case 0:
-                    break;
-                case 1: 
-                    CadastrarProduto(); 
-                    break;
-                case 2: 
-                case 3: 
-                case 4:
-                    Menu();
-                    break;
-                default: 
-                    Console.Write("Opção inválida");
-                    Menu();
-                    break;
-            }
-        }
         static void Main(string[] args)
         {
-            Menu();
+            MenuScreen.Iniciar();
         }
     }
 }
