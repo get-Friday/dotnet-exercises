a6f8cf740edf81664ed319ee6293fa93a4e8a5ec
FridayAI <gabi.get@hotmail.com>
2022-06-19 20:38:51 -0300

    implement ListarProdutoScreen

--- GeraEstoque/Screens/ListarProdutosScreen.cs
+++ GeraEstoque/Screens/ListarProdutosScreen.cs
@@ -1,4 +1,5 @@
 using GeraEstoque.Repositories;
+using GeraEstoque.Models;
 
 namespace GeraEstoque.Screens;
 
@@ -6,6 +7,12 @@ class ListarProdutosScreen
 {
     public static void Iniciar(ProdutoRepository repository)
     {
-        
+        Console.WriteLine("LISTA DE PRODUTOS CADASTRADOS");
+        Console.Write("");
+        foreach (Produto produto in repository.Produtos)
+        {
+            Console.WriteLine(produto.ToString());
+        }
+        Console.ReadLine();
     }
 }
